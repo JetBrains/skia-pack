59f3d4ea0f7f91dab1a83c14b061200a0671eb16
Nikolay Igotti <igotti@gmail.com>
2021-06-05 15:44:21 +0300

    Move to m91-1a01201b77

--- script/update_version.sh
+++ script/update_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
-OLDVER=m91-d18efa37cb
-NEWVER=m91-71fcb16fc2
+OLDVER=m91-71fcb16fc2
+NEWVER=m91-1a01201b77
 find -E $ROOT/script *.md .github  -regex '.*\.(py|md|yml)' -exec sed -i '' -e "s/$OLDVER/$NEWVER/g" {} \;
