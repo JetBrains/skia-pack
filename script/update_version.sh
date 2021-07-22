c27b3894b4af06f6a29c0e2afef0a217fcb7956d
Nikolay Igotti <igotti@gmail.com>
2021-07-22 13:10:03 +0300

    Move Skia to m92-a77a4620d2

--- script/update_version.sh
+++ script/update_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
-OLDVER=m91-1a01201b77
-NEWVER=m92-81ce29695f
+OLDVER=m92-81ce29695f
+NEWVER=m92-a77a4620d2
 find -E $ROOT/script *.md .github  -regex '.*\.(py|md|yml)' -exec sed -i '' -e "s/$OLDVER/$NEWVER/g" {} \;
