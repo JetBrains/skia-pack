dd0be8dd4bdb86be5e55a77a27e5db8c7eaa087c
Nikolay Igotti <igotti@gmail.com>
2021-08-17 19:50:46 +0400

    Fix build

--- script/update_version.sh
+++ script/update_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
-OLDVER=m92-81ce29695f
-NEWVER=m92-a77a4620d2
+OLDVER=m92-a77a4620d2
+NEWVER=m93-87e8842e8c
 find -E $ROOT/script *.md .github  -regex '.*\.(py|md|yml)' -exec sed -i '' -e "s/$OLDVER/$NEWVER/g" {} \;
