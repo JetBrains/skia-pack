edc1fd6fd9b3fd921f597e0a2484222237228381
Nikolay Igotti <igotti@gmail.com>
2021-08-17 23:12:59 +0400

    WIP

--- script/prepare_linux_arm.sh
+++ script/prepare_linux_arm.sh
@@ -8,7 +8,4 @@ apt-get install binutils build-essential -y
 apt-get install software-properties-common -y
 apt-get install python git fontconfig libfontconfig1-dev libglu1-mesa-dev curl wget -y
 apt-get install openjdk-11-jdk -y
-apt-get install clang-11 -y && \
-apt-get remove g++ -y && \
-update-alternatives --install /usr/bin/clang clang /usr/bin/clang-11 100 && \
-update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-11 100
+apt-get install clang -y
