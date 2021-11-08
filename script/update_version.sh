85b069cdcfed637fac032c4f75c5fe97f15465ce
Nikolay Igotti <igotti@gmail.com>
2021-11-08 15:16:46 +0300

    Move to Skia m97-0b898c4b8e

--- script/update_version.sh
+++ script/update_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
-OLDVER=m94-975fcdd755
-NEWVER=m96-fd5bb8d130
+OLDVER=m96-fd5bb8d130
+NEWVER=m97-0b898c4b8e
 find -E $ROOT/script *.md .github  -regex '.*\.(py|md|yml)' -exec sed -i '' -e "s/$OLDVER/$NEWVER/g" {} \;
