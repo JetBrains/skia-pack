70db69039580d9a1e2455090c951832fe16bb7e9
Nikolay Igotti <igotti@gmail.com>
2022-01-24 12:16:51 +0300

    Move to m99-51988d0bc1

--- script/update_version.sh
+++ script/update_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
-OLDVER=m96-fd5bb8d130
-NEWVER=m97-0b898c4b8e
+OLDVER=m97-0b898c4b8e
+NEWVER=m99-51988d0bc1
 find -E $ROOT/script *.md .github  -regex '.*\.(py|md|yml)' -exec sed -i '' -e "s/$OLDVER/$NEWVER/g" {} \;
