90dbf973c1555830365676824cc0cf30befa23c0
Aleksandr Veselov <veselov143@gmail.com>
2022-03-02 15:00:22 +0300

    Add patch for TextIndent implementation

--- script/update_version.sh
+++ script/update_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
-OLDVER=m97-0b898c4b8e
-NEWVER=m99-51988d0bc1
+OLDVER=m99-51988d0bc1
+NEWVER=m99-f85ab491eb
 find -E $ROOT/script *.md .github  -regex '.*\.(py|md|yml)' -exec sed -i '' -e "s/$OLDVER/$NEWVER/g" {} \;
