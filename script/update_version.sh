b2e08768fa69e146fad4fd1539a72575287916f6
Aleksandr Veselov <aleksandr.veselov@jetbrains.com>
2022-03-11 11:11:02 +0300

    Update version

--- script/update_version.sh
+++ script/update_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
-OLDVER=m99-51988d0bc1
-NEWVER=m99-f85ab491eb
+OLDVER=m99-f85ab491eb
+NEWVER=m99-f85ab491eb-2
 find -E $ROOT/script *.md .github  -regex '.*\.(py|md|yml)' -exec sed -i '' -e "s/$OLDVER/$NEWVER/g" {} \;
