557ef0bd3d6ee5a7134614760b5d528eea3e4aa4
Pavel.Sergeev <pavel.sergeev@jetbrains.com>
2022-08-03 20:47:01 +0200

    Update version

--- script/update_version.sh
+++ script/update_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
-OLDVER=m99-f85ab491eb
-NEWVER=m99-f85ab491eb-2
+OLDVER=m99-f85ab491eb-2
+NEWVER=m105-f204b137b9
 find -E $ROOT/script *.md .github  -regex '.*\.(py|md|yml)' -exec sed -i '' -e "s/$OLDVER/$NEWVER/g" {} \;
