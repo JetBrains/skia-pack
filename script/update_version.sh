6103e5ca619314f4d580617cd0b820dc07e74ac9
Pavel.Sergeev <pavel.sergeev@jetbrains.com>
2022-08-17 16:38:07 +0200

    bump version

--- script/update_version.sh
+++ script/update_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/..
-OLDVER=m105-f204b137b9
-NEWVER=m105-f204b137b9-2
+OLDVER=m105-f204b137b9-2
+NEWVER=m105-f204b137b9-5
 find -E $ROOT/script *.md .github  -regex '.*\.(py|md|yml)' -exec sed -i '' -e "s/$OLDVER/$NEWVER/g" {} \;
