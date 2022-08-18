dbe95355cc462302e5274720e15b43b12f7951dc
Oleksandr Karpovich <oleksandr.karpovich@jetbrains.com>
2022-08-18 14:48:02 +0200

    fix wasm build

--- script/prepare_linux_wasm.sh
+++ script/prepare_linux_wasm.sh
@@ -7,3 +7,14 @@ apt-get update -y
 apt-get install binutils build-essential -y
 apt-get install software-properties-common -y
 apt-get install python git curl wget -y
+
+if [ -d ./emsdk ]; then
+  cd ./emsdk
+  git pull
+else
+  git clone https://github.com/emscripten-core/emsdk.git
+  cd ./emsdk
+fi
+./emsdk install 2.0.29
+./emsdk activate 2.0.29
+source ./emsdk_env.sh
