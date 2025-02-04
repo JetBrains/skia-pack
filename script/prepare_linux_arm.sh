d7d687a67b794f22c302ba343942dfe990820217
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2025-02-04 14:44:37 +0100

    build arm in with ubuntu@22.04 docker images

--- script/prepare_linux_arm.sh
+++ script/prepare_linux_arm.sh
@@ -6,5 +6,6 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
 apt-get install binutils build-essential -y
 apt-get install software-properties-common -y
-apt-get install python3 git fontconfig libfontconfig1-dev libglu1-mesa-dev curl wget -y
+add-apt-repository ppa:git-core/ppa -y
+apt-get install python3 git fontconfig libfontconfig1-dev libglu1-mesa-dev libgl-dev curl wget -y
 apt-get install clang -y
