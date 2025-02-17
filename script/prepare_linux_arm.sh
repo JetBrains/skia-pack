7f66c0fb7a94147c4963f99f1139949d83b0e52c
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2025-02-17 18:01:49 +0100

    Attempt to declare python3-software-properties installation

--- script/prepare_linux_arm.sh
+++ script/prepare_linux_arm.sh
@@ -5,7 +5,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -y
 apt-get install binutils build-essential -y
-apt-get install python3 python-pycurl python-apt -y
+apt-get install python3 python-pycurl python-apt python3-software-properties -y
 apt-get install software-properties-common -y
 add-apt-repository ppa:git-core/ppa -y
 apt-get install git fontconfig libfontconfig1-dev libgl-dev curl wget -y
