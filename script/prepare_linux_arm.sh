60b8fd23b535186f5f1dea9ebe54fa17002444f6
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2025-02-18 16:44:23 +0100

    Smaller config for arm

--- script/prepare_linux_arm.sh
+++ script/prepare_linux_arm.sh
@@ -4,10 +4,6 @@ set -o errexit -o nounset -o pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -y
-apt-get install binutils build-essential -y
 apt-get install python3 -y
-apt-get install --fix-broken python-pycurl python-apt python3-software-properties -y
-apt-get install software-properties-common -y
-add-apt-repository ppa:git-core/ppa -y
-apt-get install git fontconfig libfontconfig1-dev libgl-dev curl wget -y
+apt-get install git fontconfig libfontconfig1-dev libgl-dev -y
 apt-get install clang -y
