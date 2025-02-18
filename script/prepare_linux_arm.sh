1ba1b29ad7b5f521a6a5df502fd7c0765c341d1a
Shagen Ogandzhanian <shagen.ogandzhanian@jetbrains.com>
2025-02-18 16:55:35 +0100

    return bin-utils, bin-essentials

--- script/prepare_linux_arm.sh
+++ script/prepare_linux_arm.sh
@@ -4,6 +4,7 @@ set -o errexit -o nounset -o pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update -y
+apt-get install binutils build-essential -y
 apt-get install python3 -y
 apt-get install git fontconfig libfontconfig1-dev libgl-dev -y
 apt-get install clang -y
