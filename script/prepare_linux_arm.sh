e1560769bb06c2ebe3536c95248cc00a44d07e93
Ivan Matkov <ivan.matkov@jetbrains.com>
2026-01-14 18:10:44 +0100

    Install python 3.9 on linux jobs

--- script/prepare_linux_arm.sh
+++ script/prepare_linux_arm.sh
@@ -7,5 +7,8 @@ apt-get update -y
 apt-get install binutils build-essential -y
 apt-get install software-properties-common -y
 add-apt-repository ppa:git-core/ppa -y
-apt-get install python3 git fontconfig libfontconfig1-dev libglu1-mesa-dev curl wget -y
+apt-get install git fontconfig libfontconfig1-dev libglu1-mesa-dev curl wget -y
 apt-get install clang -y
+
+apt-get install python3.9 -y
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 100
--- script/prepare_linux_wasm.sh
+++ script/prepare_linux_wasm.sh
@@ -6,4 +6,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update -y
 apt-get install binutils build-essential -y
 apt-get install software-properties-common -y
-apt-get install python3 git curl wget -y
+apt-get install git curl wget -y
+
+apt-get install python3.9 -y
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.9 100
